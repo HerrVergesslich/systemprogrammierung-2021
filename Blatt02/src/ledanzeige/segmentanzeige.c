733ee550edc1010ae1067d4dd8b207c74d79aa85
Mattis Fieseler <mafi1504@gmail.com>
2021-10-25 16:43:48 +0200

    added main

--- Blatt02/src/ledanzeige/segmentanzeige.c
+++ Blatt02/src/ledanzeige/segmentanzeige.c
@@ -1,3 +1,10 @@
+#include "ledanzeige/segmentanzeige.h"
+#include "ledanzeige/TM1637_intern.h"
+#include "ledanzeige/TM1637.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
 
 void TM1637_write_byte(byte wr_data){
     digitalWrite(PIN_CLOCK, LOW);
@@ -8,13 +15,3 @@ void TM1637_write_byte(byte wr_data){
     delayMicroseconds(DELAY_TIMER);
     TM1637_ack();
 }
-
-void LED_Demo(){
-    int i;
-    TM1637_setup();
-    for(i=0; i<20; i++){
-        TM1637_display_number(i);
-        delayMicroseconds(1000);
-    }
-}
-
