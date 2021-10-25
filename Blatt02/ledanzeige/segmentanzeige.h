029f9be1d3754998d2b12d6bbd46445a7e45402e
Mattis Fieseler <mafi1504@gmail.com>
2021-10-25 15:30:38 +0200

    added enums to header

--- Blatt02/ledanzeige/segmentanzeige.h
+++ Blatt02/ledanzeige/segmentanzeige.h
@@ -4,3 +4,21 @@ typedef int int16;
 typedef unsigned int uint16;
 typedef long int int32;
 typedef unsigned long int uint32;
+
+typedef enum{
+    SEG1 = 0,
+    SEG2 = 1,
+    SEG3 = 2,
+    SEG4 = 3
+}segment
+
+typedef enum {
+    OFF = 0,
+    ON = 1
+}dot
+
+typedef enum {
+    DARK = 0,
+    MEDIUM = 1,
+    BRIGHT = 7
+}brightness
