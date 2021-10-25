8e7b0683bf7af3d2487daa39203aaa32324a2d12
Finn Watermann <finn.watermann@fh-bielefeld.de>
2021-10-25 18:03:07 +0200

    Random numbers

--- Blatt02/src/ledanzeige/main.c
+++ Blatt02/src/ledanzeige/main.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <wiringPi.h>
+#include <time.h>
 
 void LED_Demo(){
     int i;
     TM1637_setup();
     for(i=0; i<20; i++){
-        TM1637_display_number((float)i);
+        TM1637_display_number((float)rand()/(float)(RAND_MAX / 999.9f));
         printf("Showing: %d\n", i);
         delayMicroseconds(1000*1000);
     }
