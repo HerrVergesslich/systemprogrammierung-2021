f43eccc0190c13da2d7986b7c8117462a0af0fd1
Finn Watermann <finn.watermann@fh-bielefeld.de>
2021-10-25 17:53:27 +0200

    Test...

--- Blatt02/src/ledanzeige/main.c
+++ Blatt02/src/ledanzeige/main.c
@@ -7,12 +7,11 @@
 void LED_Demo(){
     int i;
     TM1637_setup();
-    /*for(i=0; i<20; i++){
+    for(i=0; i<20; i++){
         TM1637_display_number(420.1f);
         printf("Showing: %d\n", i);
         delayMicroseconds(1000*1000);
-    }*/
-    TM1637_clear_display();
+    }
 }
 
 int main(){
--- Blatt02/src/ledanzeige/segmentanzeige.c
+++ Blatt02/src/ledanzeige/segmentanzeige.c
@@ -6,10 +6,19 @@
 #include <wiringPi.h>
 
 void TM1637_write_byte(byte wr_data){
+    int i;
+
     digitalWrite(PIN_CLOCK, LOW);
     delayMicroseconds(DELAY_TIMER);
-    digitalWrite(PIN_DATA, LOW);
-    delayMicroseconds(DELAY_TIMER);
+
+    for(i=0; i < 8; i ++) {
+        if(wr_data & (1 << i) > 0) {
+            digitalWrite(PIN_DATA, HIGH);
+        } else {
+            digitalWrite(PIN_DATA, LOW)
+        }
+        delayMicroseconds(DELAY_TIMER);
+    }
     digitalWrite(PIN_CLOCK, HIGH);
     delayMicroseconds(DELAY_TIMER);
     TM1637_ack();
