a6d8acb74457a638e12e1bc0565e033f20358f0f
Finn Watermann <finn.watermann@fh-bielefeld.de>
2021-10-25 16:45:47 +0200

    moved main.c to src + added include

---
+++ Blatt02/src/ledanzeige/main.c
@@ -0,0 +1,14 @@
+#include "segementanzeige.h"
+
+void LED_Demo(){
+    int i;
+    TM1637_setup();
+    for(i=0; i<20; i++){
+        TM1637_display_number(i);
+        delayMicroseconds(1000);
+    }
+}
+
+void main(){
+    LED_Demo();
+}
