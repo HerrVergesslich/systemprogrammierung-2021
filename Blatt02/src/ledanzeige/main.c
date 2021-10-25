200df8eaf2d0196afb93244a1dda3b4145da89da
Finn Watermann <finn.watermann@fh-bielefeld.de>
2021-10-25 17:37:54 +0200

    Added fixed stuff

--- Blatt02/src/ledanzeige/main.c
+++ Blatt02/src/ledanzeige/main.c
@@ -9,8 +9,8 @@ void LED_Demo(){
     TM1637_setup();
     for(i=0; i<20; i++){
         TM1637_display_number(i);
-        printf("Showing: %d", i);
-        delayMicroseconds(1000);
+        printf("Showing: %d\n", i);
+        delayMicroseconds(1000*1000);
     }
 }
 
