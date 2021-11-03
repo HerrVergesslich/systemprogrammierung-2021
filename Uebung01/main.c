aff2141a4d615e2cdf67a6687793eabc12d932eb
ITzKait0 <ben180701@gmail.com>
2021-11-03 14:01:32 +0100

    added student to linked list

--- Uebung01/main.c
+++ Uebung01/main.c
@@ -1,53 +1,82 @@
 
 
-typedef enum {
+typedef enum{
 	IFM,
 	ELM
 }enrolled;
 
-struct student {
+char* getEnumName(enrolled in){
+	switch (in)
+	{
+	case IFM:
+		return "IFM";
+		break;
+	case ELM:
+		return "ELM";
+ 	}
+}
+
+typedef struct {
 	char* name;
-	enum enrolled;
+	enrolled enrolled;
 	int cps;
-};
+}student;
 
 struct node {
-	struct node *left;
-	struct node *right;
-	struct student data;
+	struct node *prev;
+	struct node *next;
+	student data;
 };
 
-struct node * new_list() {
+struct node * new_list(student data){
     struct node *new = (struct node*) malloc(sizeof(struct node));
-    new->data = ;
-    new->left = new;
-    new->right = new;
+	new->data = data;
+    new->prev = new;
+    new->next = new;
     return new;
 };
 
-struct node * insert_right(struct node *list, int data){
+struct node * insert_next(struct node *list,student data){
     struct node *new = (struct node *) malloc(sizeof(struct node));
 	new->data        = data;
-	new->left        = list;
-	new->right       = list->right;
-	list->right      = new;
-	new->right->left = new;
+	new->prev        = list;
+	new->next       = list->next;
+	list->next      = new;
+	new->next->prev = new;
 	return new;
 }
 
+
 struct node * delete(struct node *list){
-	list->right->left = list->left;         //setzt linken Wert des rechten Nachbarn auf linken Wert von list
-	list->left->right = list->right;        //setzt rechten Wert des linken Nachbarn auf rechten Wert von list
-	return list->left;
+	list->next->prev = list->prev;         //setzt linken Wert des rechten Nachbarn auf linken Wert von list
+	list->prev->next = list->next;        //setzt rechten Wert des linken Nachbarn auf rechten Wert von list
+	return list->prev;
 }
 
-void print_all(struct node* list){
-	struct node *head = list;
-	struct node *current = list;
-	printf("%d ", head->data);
-	while (head != (current = current->right)){
-		printf("%d ", current->data);
+void print_list(struct node *list){
+	
+	for(i=0;;i++){
+		printf("name: %s | enrolled: %s | credit points: %i",list[i].data.name,getEnumName(list[i].data.enrolled),list[i].data.cps);
 	}
-	printf("\n");
 }
+struct node* getTestList(){
+	student holger = {"Holger",IFM,394};
+	student beate = {"Beate",IFM,8};
+	student heinz = {"Heinz",ELM,10};
+	struct node* main_list = new_list(holger);
+	insert_next(main_list,beate);
+	insert_next(main_list,heinz);
+	return main_list;
+}
+
+int main(){
+	struct node*main_list = getTestList();
+	print_list(main_list);
+	return 0;
+}
+
+
+
+
+
 
