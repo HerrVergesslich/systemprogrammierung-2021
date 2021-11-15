767a5d11fe23b7e0d0ee23a12ac21c872e12ae0d
Finn Watermann <finn.watermann@fh-bielefeld.de>
2021-11-16 00:35:14 +0100

    Solving Tests

--- Blatt04/src/speicherverwaltung/speicherverwaltung.c
+++ Blatt04/src/speicherverwaltung/speicherverwaltung.c
@@ -1,56 +1,57 @@
 #include "speicherverwaltung/speicherverwaltung.h"
 #include <stdio.h>
-char mempool[MEM_POOL_SIZE];
-memblock * freemem;
 
-int cm_init(void){
-    /*printf("Size of memblock pointer: %d\n", sizeof(memblock*));
-    printf("Size of memblock: %d\n", sizeof(memblock));*/
-    static int called = 0;
-    if(MEM_POOL_SIZE<4096){
-        return -1;
-    }else if(called != 0){
-        return 0;
-    }
-    called = 1;
-    freemem = (memblock *)mempool;
-    freemem->size = (MEM_POOL_SIZE-sizeof(memblock));
+char mempool[MEM_POOL_SIZE];
+memblock* freemem;
+
+int cm_init(void) {
+    static int alreadyCalled = 0;
+
+    if(alreadyCalled) return 0;
+    if(MEM_POOL_SIZE < 4096) return -1;
+
+    alreadyCalled = 1;
+
+    freemem = mempool;
     freemem->id = 0;
+    freemem->size = MEM_POOL_SIZE - sizeof(memblock);
     freemem->next = NULL;
+
     return 1;
 }
 
-void * cm_malloc(size_t needSize){
-    static int number = 1;
-    
+void* cm_malloc(size_t needSize) {
+
     if(needSize <= 0) return NULL;
-    if(freemem == NULL) return NULL;
 
+    static unsigned int nextId = 1;
     memblock* run = freemem;
-    
-    while(run != NULL){
+    memblock* prev = NULL;
 
-        if(run->size >= needSize){
+    do {
+        if(run->size >= needSize) { //Block ist groß genug für benötigten Speicherplatz
+            
+            if(prev != NULL) prev->next = run->next;
+            if(run == freemem) freemem = freemem->next;
 
-            run->id = number++; 
-            run->next = (memblock*) MAGIC_INT;
-
-            if(freemem->size - run->size <= 0) freemem = NULL;
-            printf("Size of free block: %d", run->size);
-
+            run->id = nextId ++;
+            run->next = MAGIC_INT;
+            
             return run+1;
+        } else {
+            prev = run;
+            run = run->next;
         }
+    } while(run->next != NULL);
 
-        run = run->next;
-    }
     return NULL;
 }
 
- void cm_free(void *ptr){
+void cm_free(void *ptr){
     memblock * temp = (memblock *) ptr;
     if(temp && temp->next == (memblock *)MAGIC_INT){
          temp->next = freemem;
-         temp->id = 1;
+         temp->id = 0;
          freemem = temp;
     }
 }
