18f1ac5933f006f23bb4d155c7f7c3fa306be353
ITzKait0 <ben180701@gmail.com>
2021-11-21 20:30:02 +0100

    edited ringbuffer.h in  Blatt05

--- Blatt05/include/ringbuffer/ringbuffer.h
+++ Blatt05/include/ringbuffer/ringbuffer.h
@@ -1,3 +1,11 @@
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
+struct ring_buffer{
+    size_t size,count,head;
+    void **elems;
+    void (*free_callback)(void *p);
+};
+
+void write();
+void init_buffer();
